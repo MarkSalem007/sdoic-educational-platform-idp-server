3a847135c6c1a01e02bac311b646a24b4cc8d242
MarkSalem007 <immarkjosephvalenzuela@gmail.com>
2026-08-24 14:34:07 +0800

    Updated

--- src/utils/cookies.js
+++ src/utils/cookies.js
@@ -2,14 +2,15 @@ import env from '../config/env.js';
 import { durationToMilliseconds } from './duration.js';
 
 const isProduction = env.NODE_ENV === 'production';
+const isDepedDomain = env.APP_URL?.includes('depedimuscity.com') || env.ALLOWED_CALLBACK_DOMAINS?.some(d => d.includes('depedimuscity.com'));
+const cookieDomain = env.COOKIE_DOMAIN || (isProduction || isDepedDomain ? '.depedimuscity.com' : undefined);
+
 const accessTokenMaxAge = durationToMilliseconds(env.JWT_ACCESS_EXPIRES_IN);
 const refreshTokenMaxAge = durationToMilliseconds(env.JWT_REFRESH_EXPIRES_IN);
 
-const cookieDomain = env.COOKIE_DOMAIN || (isProduction ? '.depedimuscity.com' : undefined);
-
 const cookieOptions = {
     httpOnly: true,
-    secure: isProduction,
+    secure: isProduction || isDepedDomain,
     sameSite: 'lax',
     path: '/',
     ...(cookieDomain ? { domain: cookieDomain } : {}),
