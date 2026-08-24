1281ef332b5be6ff72a081863097edb2ac4f3e8e
MarkSalem007 <immarkjosephvalenzuela@gmail.com>
2026-08-24 14:43:23 +0800

    Updated fix 2'

--- src/utils/cookies.js
+++ src/utils/cookies.js
@@ -2,18 +2,15 @@ import env from '../config/env.js';
 import { durationToMilliseconds } from './duration.js';
 
 const isProduction = env.NODE_ENV === 'production';
-const isDepedDomain = env.APP_URL?.includes('depedimuscity.com') || env.ALLOWED_CALLBACK_DOMAINS?.some(d => d.includes('depedimuscity.com'));
-const cookieDomain = env.COOKIE_DOMAIN || (isProduction || isDepedDomain ? '.depedimuscity.com' : undefined);
-
 const accessTokenMaxAge = durationToMilliseconds(env.JWT_ACCESS_EXPIRES_IN);
 const refreshTokenMaxAge = durationToMilliseconds(env.JWT_REFRESH_EXPIRES_IN);
 
 const cookieOptions = {
     httpOnly: true,
-    secure: isProduction || isDepedDomain,
+    secure: isProduction,
     sameSite: 'lax',
     path: '/',
-    ...(cookieDomain ? { domain: cookieDomain } : {}),
+    ...(isProduction ? { domain: env.COOKIE_DOMAIN || '.depedimuscity.com' } : {}),
 };
 
 export const setAccessTokenCookie = (res, token) => {
